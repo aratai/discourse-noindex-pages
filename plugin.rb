dcc631e2037eb33af4623e612b00242cd6ce87bb
aratai <lubomirgeci2@gmail.com>
2025-07-21 11:10:07 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,45 +6,28 @@
 # authors: Tvoj Nick
 
 after_initialize do
-  Rails.logger.info "[SEO Plugin] Initializing SEO improvements plugin"
 
-  # Pridáme custom meta tagy do hlavičky
-  register_html_builder('server:before-head-close') do |controller|
-    if controller.instance_of?(TopicsController)
-      Rails.logger.debug "[SEO Plugin] Processing TopicsController request"
-      
-      topic_view = controller.instance_variable_get(:@topic_view)
-      return '' unless topic_view&.topic
+  add_to_serializer(:topic_view, :seo_meta_tags) do
+    return '' unless object.topic
 
-      url = controller.request.fullpath
-      page = controller.params[:page].to_i
-      
-      Rails.logger.debug "[SEO Plugin] URL: #{url}, Page: #{page}"
-      
-      has_post_number = url.match?(/\/\d+\/\d+$/)
-      is_paged = page > 1
+    url = scope.request.fullpath
+    is_paged = object.instance_variable_get(:@params)[:page].to_i > 1
+    has_post_number = url.match?(/\/\d+\/\d+$/) || scope.params[:post_number].present?
 
-      result = ''
-      
-      if is_paged || has_post_number
-        Rails.logger.info "[SEO Plugin] Adding noindex for URL: #{url}"
-        result += '<meta name="robots" content="noindex, follow">'
-      end
+    meta = ""
 
-      # Nastavíme canonical URL
-      base_url = Discourse.base_url
-      topic_url = topic_view.topic.relative_url
-      clean_url = topic_url.split('?').first.split('/').reject { |part| part.match?(/^\d+$/) }.join('/')
-      canonical = "#{base_url}#{clean_url}"
-      
-      Rails.logger.debug "[SEO Plugin] Setting canonical URL: #{canonical}"
-      result += %Q(<link rel="canonical" href="#{canonical}" />)
-
-      result
+    if is_paged || has_post_number
+      Rails.logger.info "[SEO Plugin] Adding noindex to: #{url}"
+      meta += '<meta name="robots" content="noindex, follow">'
     else
-      ''
+      Rails.logger.info "[SEO Plugin] Noindex not needed for: #{url}"
     end
+
+    canonical = "#{Discourse.base_url}#{object.topic.relative_url}"
+    Rails.logger.info "[SEO Plugin] Setting canonical: #{canonical}"
+    meta += %Q(<link rel="canonical" href="#{canonical}" />)
+
+    meta
   end
 
-  Rails.logger.info "[SEO Plugin] SEO improvements plugin initialized successfully"
 end
