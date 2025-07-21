cd40854028555b1d5ec7adfa23b2ecee2642812b
aratai <lubomirgeci2@gmail.com>
2025-07-21 02:03:14 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@
 after_initialize do
 
   add_to_serializer(:topic_view, :extra_noindex) do
-    if scope.is_a?(Guardian) && scope.request.format == :html && object.topic
+    if scope.is_a?(Guardian) && scope.request.format == :html && object.respond_to?(:topic) && object.topic
       url = scope.request.fullpath
       has_post_number = url.match(/\/\d+\/\d+$/)
       is_paged = object.instance_variable_get(:@params)[:page].to_i > 1
@@ -24,19 +24,20 @@ after_initialize do
   end
 
   add_to_serializer(:topic_view, :canonical_url) do
-    if scope.is_a?(Guardian) && scope.request.format == :html && object.topic
+    if scope.is_a?(Guardian) && scope.request.format == :html && object.respond_to?(:topic) && object.topic
       params = object.instance_variable_get(:@params)
       has_post_number = scope.request.fullpath.match(/\/\d+\/\d+$/)
       is_paged = params[:page].to_i > 1
 
       if is_paged || has_post_number
-        object.topic.relative_url
+        Discourse.base_url_no_prefix + object.topic.relative_url
       else
-        object.canonical_url
+        Discourse.base_url_no_prefix + object.topic.relative_url
       end
     else
-      object.canonical_url
+      Discourse.base_url_no_prefix + object.topic.relative_url
     end
   end
 
 end
+
