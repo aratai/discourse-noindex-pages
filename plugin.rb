26a8466196c27228866bf0ef14ee983d2eb7e591
aratai <lubomirgeci2@gmail.com>
2025-07-29 18:53:03 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,37 +7,40 @@
 
 after_initialize do
 
-  module ::TopicsControllerSEO
-    def show
-      super
+module ::TopicsControllerSEO
+  def show
+    super
 
-      # Nastav canonical hlavičku
-      url = request.fullpath
-      if url =~ %r{^/t/([^/]+)/(\d+)}
-        slug = $1
-        topic_id = $2
-        canonical_url = "https://infrastruktura.sk/t/#{slug}/#{topic_id}"
-        response.headers["Link"] = "<#{canonical_url}>; rel=\"canonical\""
-        Rails.logger.warn("CANONICAL SET: #{canonical_url}")
+    url = request.fullpath
+    if url =~ %r{^/t/([^/]+)/(\d+)}
+      slug = $1
+      topic_id = $2
+      canonical_url = "https://infrastruktura.sk/t/#{slug}/#{topic_id}"
+      response.headers["Link"] = "<#{canonical_url}>; rel=\"canonical\""
+      Rails.logger.warn("CANONICAL SET: #{canonical_url}")
+    end
+
+    if @topic
+      archive_root_id = 40
+      topic_cid = @topic.category_id
+      Rails.logger.warn("TOPIC #{@topic.id} has category_id: #{topic_cid}")
+
+      archive_ids = Category.where(parent_category_id: archive_root_id).pluck(:id)
+      archive_ids << archive_root_id
+      Rails.logger.warn("ARCHIVE IDS: #{archive_ids}")
+
+      if archive_ids.include?(topic_cid)
+        response.headers["X-Robots-Tag"] = "noindex, follow"
+        Rails.logger.warn("NOINDEX APPLIED: Topic #{@topic.id}")
+      else
+        Rails.logger.warn("NOINDEX NOT APPLIED: Topic #{@topic.id} category_id #{topic_cid} not in archive_ids")
       end
-
-      # Nastav noindex pre archiv
-      if @topic
-        archive_root_id = 40
-
-        # Získaj všetky ID podkategórií archívu
-        archive_ids = Category.where(parent_category_id: archive_root_id).pluck(:id)
-        archive_ids << archive_root_id
-
-        if archive_ids.include?(@topic.category_id)
-          response.headers["X-Robots-Tag"] = "noindex, follow"
-          Rails.logger.warn("NOINDEX APPLIED: Topic #{@topic.id} in archive by ID")
-        else
-          Rails.logger.warn("NOINDEX SKIPPED: Topic #{@topic.id} not in archive")
-        end
-      end
+    else
+      Rails.logger.warn("NO @topic present in TopicsController#show")
     end
   end
+end
+
 
   ::TopicsController.prepend ::TopicsControllerSEO
 
