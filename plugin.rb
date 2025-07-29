3b8384b02281fe50e6c4381a7286dac5c2fd9898
aratai <lubomirgeci2@gmail.com>
2025-07-29 18:37:07 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,32 +1,35 @@
 # frozen_string_literal: true
 
 # name: discourse-noindex-pages
-# about: Adds noindex for archive category and canonical headers for paginated/topic-post URLs
+# about: Adds noindex for archive topics and canonical header for topic pagination
 # version: 0.1
 # authors: Tvoj Nick
 
 after_initialize do
 
-  # NOINDEX pre archiv-monitoringu-tlace a jeho podkategórie
+  # NOINDEX pre archiv a podkategorie
   module ::TopicsControllerNoindexAuto
     def show
       super
 
       if @topic
-        archive_parent_slug = "archiv-monitoringu-tlace"
-        topic_category = @topic.category
+        archive_root = Category.find_by(slug: "archiv-monitoringu-tlace")
 
-        if topic_category &&
-           (topic_category.slug == archive_parent_slug ||
-            topic_category.ancestors.any? { |c| c.slug == archive_parent_slug })
+        if archive_root && @topic.category
+          archive_ids = [archive_root.id] + archive_root.subcategories.pluck(:id)
 
-          response.headers["X-Robots-Tag"] = "noindex, follow"
+          if archive_ids.include?(@topic.category_id)
+            response.headers["X-Robots-Tag"] = "noindex, follow"
+            Rails.logger.warn("NOINDEX APPLIED: Topic #{@topic.id} in archive")
+          else
+            Rails.logger.warn("NOINDEX SKIPPED: Topic #{@topic.id} not in archive")
+          end
         end
       end
     end
   end
 
-  # CANONICAL pre všetky stránkované alebo konkrétne posty
+  # CANONICAL pre paginované alebo post-specific URL
   module ::TopicsControllerCanonicalAuto
     def show
       super
@@ -37,14 +40,17 @@ after_initialize do
         topic_id = $2
         canonical_url = "https://infrastruktura.sk/t/#{slug}/#{topic_id}"
         response.headers["Link"] = "<#{canonical_url}>; rel=\"canonical\""
+
+        Rails.logger.warn("CANONICAL SET: #{canonical_url}")
       end
     end
   end
 
+  # Prepni oba patche
   ::TopicsController.prepend ::TopicsControllerNoindexAuto
   ::TopicsController.prepend ::TopicsControllerCanonicalAuto
 
-  # Odstráni <link rel="canonical"> z HTML hlavičky (necháme len HTTP header)
+  # Odstráni <link rel="canonical"> z HTML <head>, necháme len v hlavičke
   module ::CanonicalURL::Helpers
     def canonical_link_tag(url = nil)
       ""
