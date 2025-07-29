988fc94d963daab5be31116ae4e7206a6dff987b
aratai <lubomirgeci2@gmail.com>
2025-07-29 18:23:32 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,36 +1,54 @@
 # frozen_string_literal: true
 
 # name: discourse-noindex-pages
-# about: Adds noindex and canonical fix for topic subpages and specific post links
+# about: Adds noindex for archive category and canonical headers for paginated/topic-post URLs
 # version: 0.1
 # authors: Tvoj Nick
 
 after_initialize do
 
-  # Patchne TopicsController, aby pridal canonical do HTTP hlavičky
+  # NOINDEX pre archiv-monitoringu-tlace a jeho podkategórie
+  module ::TopicsControllerNoindexAuto
+    def show
+      super
+
+      if @topic
+        archive_parent_slug = "archiv-monitoringu-tlace"
+        topic_category = @topic.category
+
+        if topic_category &&
+           (topic_category.slug == archive_parent_slug ||
+            topic_category.ancestors.any? { |c| c.slug == archive_parent_slug })
+
+          response.headers["X-Robots-Tag"] = "noindex, follow"
+        end
+      end
+    end
+  end
+
+  # CANONICAL pre všetky stránkované alebo konkrétne posty
   module ::TopicsControllerCanonicalAuto
     def show
       super
 
       url = request.fullpath
-      # Nájde slug a topic_id z URL: /t/slug/123 alebo /t/slug/123?page=2 atď.
       if url =~ %r{^/t/([^/]+)/(\d+)}
         slug = $1
         topic_id = $2
         canonical_url = "https://infrastruktura.sk/t/#{slug}/#{topic_id}"
-
-        # Pridaj do HTTP hlavičky
         response.headers["Link"] = "<#{canonical_url}>; rel=\"canonical\""
       end
     end
   end
 
+  ::TopicsController.prepend ::TopicsControllerNoindexAuto
   ::TopicsController.prepend ::TopicsControllerCanonicalAuto
-  
-  module CanonicalURL::Helpers
-     def canonical_link_tag(url = nil)
-        ""
-     end
+
+  # Odstráni <link rel="canonical"> z HTML hlavičky (necháme len HTTP header)
+  module ::CanonicalURL::Helpers
+    def canonical_link_tag(url = nil)
+      ""
+    end
   end
 
 end
