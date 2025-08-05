91715eed1591ab4efbfc1338e4736eb6f9619a49
aratai <lubomirgeci2@gmail.com>
2025-08-06 01:53:23 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,30 +6,35 @@
 # authors: Tvoj Nick
 
 after_initialize do
-module ::TopicsControllerSEO
-  def show
-    # --- Kontrola pagináciách PRED super ---
-    url = request.fullpath
-    is_pagination = url.include?('?page=') || url =~ %r{^/t/[^/]+/\d+/\d+}
-    
-    # Pre paginácie: vynútiť 200 namiesto 304 pre Googlebot
-    if is_pagination && request.user_agent&.include?('Googlebot')
-      request.headers.delete('If-Modified-Since')
-      request.headers.delete('If-None-Match')
-      Rails.logger.warn("FORCED 200 FOR GOOGLEBOT PAGINATION: #{url}")
-    end
-    
-    super
-    
-    # --- Noindex pre paginácie ---
-    if is_pagination
-      response.headers["X-Robots-Tag"] = "noindex, follow"
-      response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-      Rails.logger.warn("NOINDEX PAGINATION: #{url}")
-    else
-      Rails.logger.warn("MAIN TOPIC PAGE: #{url}")
+  module ::TopicsControllerSEO
+    def show
+      url = request.fullpath
+      is_pagination = url.include?('?page=') || url =~ %r{^/t/[^/]+/\d+/\d+}
+
+      if is_pagination && request.user_agent&.include?('Googlebot')
+        # Bezpečne odstrániť cache hlavičky
+        request.env.delete('HTTP_IF_MODIFIED_SINCE')
+        request.env.delete('HTTP_IF_NONE_MATCH')
+        Rails.logger.warn("FORCED 200 FOR GOOGLEBOT PAGINATION: #{url}")
+      end
+
+      super
+
+      if is_pagination
+        response.headers["X-Robots-Tag"] = "noindex, follow"
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        Rails.logger.warn("NOINDEX PAGINATION: #{url}")
+      else
+        Rails.logger.warn("MAIN TOPIC PAGE: #{url}")
+      end
     end
   end
-end
-  ::TopicsController.prepend ::TopicsControllerSEO
+
+  if defined?(::TopicsController)
+    ::TopicsController.prepend ::TopicsControllerSEO
+  else
+    DiscourseEvent.on(:controllers_loaded) do
+      ::TopicsController.prepend ::TopicsControllerSEO
+    end
+  end
 end
