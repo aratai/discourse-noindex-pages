d193571743b07cb1a90417b233d8bc7041db044a
aratai <lubomirgeci2@gmail.com>
2025-11-28 01:11:21 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,7 @@
 
 after_initialize do
   ##########################################
-  # 1) NOINDEX FOR PAGINATED TOPICS
+  # 1) NOINDEX PRE PAGINÁCIU TOPICOV
   ##########################################
   module ::TopicsControllerSEO
     def show
@@ -15,6 +15,7 @@ after_initialize do
       is_pagination = url.include?("?page=") || url =~ %r{^/t/[^/]+/\d+/\d+}
 
       if is_pagination && request.user_agent&.include?("Googlebot")
+        # zruš ETag/If-Modified-Since, nech má Googlebot vždy 200
         request.env.delete("HTTP_IF_MODIFIED_SINCE")
         request.env.delete("HTTP_IF_NONE_MATCH")
       end
@@ -35,95 +36,85 @@ after_initialize do
   end
 
   ##########################################
-  # 2) FACEBOOK VIDEO OEMBED DEBUG LOGGING
+  # 2) FACEBOOK VIDEO OEMBED + DEBUG LOGY
   ##########################################
 
-  require "uri"
-  require "multi_json"
+  # Toto JE dôležité: najprv načítať originálny engine z core
+  require_dependency "onebox/engine/facebook_media_onebox"
 
-  module ::Onebox
-    module Engine
-      class FacebookMediaOnebox
+  ::Onebox::Engine::FacebookMediaOnebox.class_eval do
+    # helper na logovanie, nech to vieme ľahko filtrovať
+    def fb_log(msg)
+      Rails.logger.warn("[FB OEMBED] #{msg}")
+    end
 
-        # pridáme log z konštruktora, aby sme vedeli, že engine sa načítal
-        def initialize(url, options)
-          Rails.logger.warn("[FB OEMBED] FacebookMediaOnebox INITIALIZED for #{url}")
-          super
-        end
+    def oembed_html
+      token = SiteSetting.facebook_app_access_token
 
-        def placeholder_html
-          ::Onebox::Helpers.video_placeholder_html
-        end
-
-        def oembed_html
-          token = SiteSetting.facebook_app_access_token
-
-          if token.blank?
-            Rails.logger.warn("[FB OEMBED] TOKEN EMPTY – skipping oEmbed for #{url}")
-            return nil
-          end
-
-          Rails.logger.warn("[FB OEMBED] Trying Graph API for #{url}")
-          query = URI.encode_www_form(url: url, access_token: token)
-          oembed_url = "https://graph.facebook.com/v24.0/oembed_video?#{query}"
-
-          Rails.logger.warn("[FB OEMBED] Request URL: #{oembed_url}")
-
-          response = ::Onebox::Helpers.fetch_response(oembed_url)
-          Rails.logger.warn("[FB OEMBED] HTTP #{response.code} for #{url}")
-
-          body = response.body.to_s
-          Rails.logger.warn("[FB OEMBED] RESPONSE BODY (first 300 chars): #{body[0..300]}")
-
-          data = ::MultiJson.load(body)
-          html = data["html"].to_s
-
-          if html.empty?
-            Rails.logger.warn("[FB OEMBED] EMPTY HTML returned from Graph API")
-            return nil
-          end
-
-          Rails.logger.warn("[FB OEMBED] HTML RECEIVED (length=#{html.length})")
-
-          html.gsub('src="https:/', 'src="https://')
-        rescue => e
-          Rails.logger.warn("[FB OEMBED] ERROR for #{url}: #{e.class} – #{e.message}")
-          Rails.logger.warn(e.backtrace.join("\n"))
-          nil
-        end
-
-        def to_html
-          Rails.logger.warn("[FB OEMBED] to_html CALLED for #{url}")
-
-          if (html = oembed_html).present?
-            Rails.logger.warn("[FB OEMBED] SUCCESS – using FB Graph oEmbed for #{url}")
-            return html
-          end
-
-          Rails.logger.warn("[FB OEMBED] FALLBACK – oEmbed failed for #{url}, trying twitter player")
-
-          metadata = get_twitter
-
-          if metadata.present? && metadata[:card] == "player"
-            Rails.logger.warn("[FB OEMBED] Using twitter player fallback for #{url}")
-
-            return <<~HTML
-              <iframe
-                src="#{metadata[:player]}"
-                width="#{metadata[:player_width]}"
-                height="#{metadata[:player_height]}"
-                scrolling="no"
-                frameborder="0"
-                allowfullscreen="true">
-              </iframe>
-            HTML
-          end
-
-          Rails.logger.warn("[FB OEMBED] FINAL FALLBACK – using generic OG onebox for #{url}")
-          ::Onebox::Engine::AllowlistedGenericOnebox.new(url, @options || {}).to_html
-        end
-
+      if token.blank?
+        fb_log("TOKEN EMPTY – skipping oEmbed for #{url}")
+        return nil
       end
+
+      fb_log("Trying Graph API for #{url}")
+      query = URI.encode_www_form(url: url, access_token: token)
+      oembed_url = "https://graph.facebook.com/v24.0/oembed_video?#{query}"
+
+      fb_log("Request URL: #{oembed_url}")
+
+      response = ::Onebox::Helpers.fetch_response(oembed_url)
+      fb_log("HTTP #{response.code} for #{url}")
+
+      body = response.body.to_s
+      fb_log("RESPONSE BODY (first 300 chars): #{body[0..300]}")
+
+      data = ::MultiJson.load(body)
+      html = data["html"].to_s
+
+      if html.empty?
+        fb_log("EMPTY HTML returned from Graph API")
+        return nil
+      end
+
+      fb_log("HTML RECEIVED (length=#{html.length})")
+
+      # fix na prípadné rozbité "https:/"
+      html.gsub('src="https:/', 'src="https://')
+    rescue => e
+      fb_log("ERROR for #{url}: #{e.class} – #{e.message}")
+      fb_log(e.backtrace.join("\n"))
+      nil
+    end
+
+    def to_html
+      fb_log("to_html CALLED for #{url}")
+
+      if (html = oembed_html).present?
+        fb_log("SUCCESS – using FB Graph oEmbed for #{url}")
+        return html
+      end
+
+      fb_log("FALLBACK – oEmbed failed for #{url}, trying twitter player")
+
+      metadata = get_twitter
+
+      if metadata.present? && metadata[:card] == "player" && metadata[:player].present?
+        fb_log("Using twitter player fallback for #{url}")
+
+        return <<~HTML
+          <iframe
+            src="#{metadata[:player]}"
+            width="#{metadata[:player_width]}"
+            height="#{metadata[:player_height]}"
+            scrolling="no"
+            frameborder="0"
+            allowfullscreen="true">
+          </iframe>
+        HTML
+      end
+
+      fb_log("FINAL FALLBACK – using generic OG onebox for #{url}")
+      ::Onebox::Engine::AllowlistedGenericOnebox.new(url, @options || {}).to_html
     end
   end
 end
