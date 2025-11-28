d0bdda4060a48f00eda95eb3510cea68c441a237
aratai <lubomirgeci2@gmail.com>
2025-11-28 15:56:04 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -43,52 +43,62 @@ after_initialize do
   require "uri"
   require_dependency "onebox/engine/facebook_media_onebox"
 
-  ::Onebox::Engine::FacebookMediaOnebox.class_eval do
-    # Povieme oneboxu, že vraciame hotové HTML (nie URL na oEmbed)
-    def has_html?
-      true
-    end
-
-    def to_html
-      href = url
-
-      # Rozhodneme, či je to video / reel / share, alebo obyčajný post
-      is_video =
-        href =~ /\/videos\//i ||
-        href.include?("/reel/") ||
-        href.include?("/share/v/")
-
-      base =
-        if is_video
-          "https://www.facebook.com/plugins/video.php"
-        else
-          "https://www.facebook.com/plugins/post.php"
-        end
-
-      # Rovnaké čísla, ako ručne vložený iframe
-      width  = 500
-      height = is_video ? 281 : 576
-
-      params = {
-        "href"      => href,
-        "show_text" => "true",
-        "width"     => width
-      }
-
-      src = "#{base}?#{URI.encode_www_form(params)}"
-
-      <<~HTML
-        <iframe
-          src="#{src}"
-          width="#{width}"
-          height="#{height}"
-          style="border:none;overflow:hidden"
-          scrolling="no"
-          frameborder="0"
-          allowfullscreen="true"
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
-        </iframe>
-      HTML
-    end
+::Onebox::Engine::FacebookMediaOnebox.class_eval do
+  def has_html?
+    true
   end
+
+  def to_html
+    href = url
+
+    is_reel =
+      href.include?("/reel/")
+
+    is_video =
+      href =~ /\/videos\//i || href.include?("/share/v/") || is_reel
+
+    base =
+      if is_video
+        "https://www.facebook.com/plugins/video.php"
+      else
+        "https://www.facebook.com/plugins/post.php"
+      end
+
+    # Rozumné defaulty
+    if is_reel
+      # Pomer podľa FB príkladu: 267x591 ~ 1:2.2
+      width  = 320    # šírka na fóre
+      height = (width * 2.2).to_i
+    elsif is_video
+      width  = 500
+      height = 281    # približne 16:9
+    else
+      width  = 500
+      height = 576    # post s textom
+    end
+
+    params = {
+      "href"       => href,
+      "show_text"  => "true",
+      "width"      => width,
+      "height"     => height, # FB to má rád v src
+      "t"          => 0,      # presne ako FB embed
+    }
+
+    src = "#{base}?#{URI.encode_www_form(params)}"
+
+    <<~HTML
+      <iframe
+        src="#{src}"
+        width="#{width}"
+        height="#{height}"
+        style="border:none;overflow:hidden"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+      </iframe>
+    HTML
+  end
+end
 end
