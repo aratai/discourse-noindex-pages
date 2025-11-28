1f75ba776b27637f0deb9289f3ad2e8e10325aba
aratai <lubomirgeci2@gmail.com>
2025-11-28 01:47:59 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -43,80 +43,38 @@ after_initialize do
   # 2) FACEBOOK VIDEO OEMBED + DEBUG LOGY
   ##########################################
 
-  # Toto JE dôležité: najprv načítať originálny engine z core
+    require "cgi"
   require_dependency "onebox/engine/facebook_media_onebox"
 
   ::Onebox::Engine::FacebookMediaOnebox.class_eval do
-    # helper na logovanie, nech to vieme ľahko filtrovať
-    def fb_log(msg)
-      Rails.logger.warn("[FB OEMBED] #{msg}")
-    end
-
-    def oembed_html
-      token = SiteSetting.facebook_app_access_token
-
-      if token.blank?
-        fb_log("TOKEN EMPTY – skipping oEmbed for #{url}")
-        return nil
-      end
-
-      fb_log("Trying Graph API for #{url}")
-      query = URI.encode_www_form(url: url, access_token: token)
-      oembed_url = "https://graph.facebook.com/v24.0/oembed_video?#{query}"
-
-      fb_log("Request URL: #{oembed_url}")
-
-      # fetch_response v Discourse vráti priamo BODY ako string
-      body = ::Onebox::Helpers.fetch_response(oembed_url).to_s
-      fb_log("RESPONSE BODY (first 300 chars): #{body[0..300]}")
-
-      data = ::MultiJson.load(body)
-      html = data["html"].to_s
-
-      if html.empty?
-        fb_log("EMPTY HTML returned from Graph API – parsed JSON keys: #{data.keys.join(", ")}")
-        return nil
-      end
-
-      fb_log("HTML RECEIVED (length=#{html.length})")
-
-      # fix na prípadné rozbité "https:/"
-      html.gsub('src="https:/', 'src="https://')
-    rescue => e
-      fb_log("ERROR for #{url}: #{e.class} – #{e.message}")
-      fb_log(e.backtrace.join("\n"))
-      nil
-    end
-
     def to_html
-      fb_log("to_html CALLED for #{url}")
+      href = url
 
-      if (html = oembed_html).present?
-        fb_log("SUCCESS – using FB Graph oEmbed for #{url}")
-        return html
-      end
+      # Rozhodneme, či ide o video, reel, share, alebo bežný post
+      base =
+        if href =~ /\/videos\//i || href.include?("/reel/") || href.include?("/share/v/")
+          "https://www.facebook.com/plugins/video.php"
+        else
+          "https://www.facebook.com/plugins/post.php"
+        end
 
-      fb_log("FALLBACK – oEmbed failed for #{url}, trying twitter player")
+      width  = 500
+      height = 281
 
-      metadata = get_twitter
+      src = "#{base}?href=#{CGI.escape(href)}&show_text=true&width=#{width}"
 
-      if metadata.present? && metadata[:card] == "player" && metadata[:player].present?
-        fb_log("Using twitter player fallback for #{url}")
-
-        return <<~HTML
-          <iframe
-            src="#{metadata[:player]}"
-            width="#{metadata[:player_width]}"
-            height="#{metadata[:player_height]}"
-            scrolling="no"
-            frameborder="0"
-            allowfullscreen="true">
-          </iframe>
-        HTML
-      end
-
-      fb_log("FINAL FALLBACK – using generic OG onebox for #{url}")
-      ::Onebox::Engine::AllowlistedGenericOnebox.new(url, @options || {}).to_html
+      <<~HTML
+        <iframe
+          src="#{src}"
+          width="#{width}"
+          height="#{height}"
+          style="border:none;overflow:hidden"
+          scrolling="no"
+          frameborder="0"
+          allowfullscreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+        </iframe>
+      HTML
     end
   end
 end
